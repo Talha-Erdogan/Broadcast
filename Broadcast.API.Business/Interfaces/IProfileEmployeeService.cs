6b81e23e222cbbbe9b58030706a119e74e5ec6dc
talha erdoğan <talha_erd_91@hotmail.com>
2020-08-05 14:50:16 +0300

    Business kodlanmaya devam ediliyor.

---
+++ Broadcast.API.Business/Interfaces/IProfileEmployeeService.cs
@@ -0,0 +1,20 @@
+﻿using Broadcast.API.Business.Models;
+using Broadcast.API.Business.Models.ProfileEmployee;
+using Broadcast.API.Data.Entity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Broadcast.API.Business.Interfaces
+{
+    public interface IProfileEmployeeService
+    {
+        PaginatedList<Employee> GetAllEmployeePaginatedWithDetailBySearchFilter(ProfileEmployeeSearchFilter searchFilter);
+        PaginatedList<Employee> GetAllEmployeeWhichIsNotIncludedPaginatedWithDetailBySearchFilter(ProfileEmployeeSearchFilter searchFilter);
+        List<Profile> GetAllProfileByCurrentUser(int employeeId);
+        List<Profile> GetAllProfileByEmployeeIdAndAuthCode(int employeeId, string authCode);
+        int Add(ProfileEmployee record);
+        int DeleteByProfileIdAndEmployeeId(int profileId, int employeeId);
+
+    }
+}
---
+++ Broadcast.API.Business/Interfaces/IProfileService.cs
@@ -0,0 +1,18 @@
+﻿using Broadcast.API.Business.Models;
+using Broadcast.API.Business.Models.Profile;
+using Broadcast.API.Data.Entity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Broadcast.API.Business.Interfaces
+{
+    public interface IProfileService
+    {
+        PaginatedList<Profile> GetAllPaginatedWithDetailBySearchFilter(ProfileSearchFilter searchFilter);
+        List<Profile> GetAll();
+        Profile GetById(int id);
+        int Add(Profile record);
+        int Update(Profile record);
+    }
+}
